0e748bb1d1120d8ea368ac050db895c9726333d8
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-29 22:28:14 +0500

    feat:Config Database

---
+++ configs/connectdb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const connectDb = async (DATABASE_URL) => {
+  try {
+    const DB_OPTION = {
+      dbName: "AuthenticationApi",
+    };
+    await mongoose.connect(DATABASE_URL, DB_OPTION);
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+export default connectDb;
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import connectDb from "./configs/connectdb.js";
 
-const app = express();
+// dotenv config
 dotenv.config();
 
+const app = express();
 // Cors policy issue on frontend fixing
 app.use(cors());
 
 const port = process.env.PORT;
+const databaseUrl = process.env.DATABASE_URL;
+// data base connecting
+connectDb(databaseUrl);
+
 app.listen(port, () => {
   console.log(`Port running at https://localhost:${port}`);
 });
