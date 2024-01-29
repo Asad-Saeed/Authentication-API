83700c58222fd95779e2b76a2cf0a6bd28f45150
Asad Saeed <asadsaeed12225@gmail.com>
2024-01-29 21:49:30 +0500

    feat:Config CORS Policy

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+
 const app = express();
 dotenv.config();
+
+// Cors policy issue on frontend fixing
+app.use(cors());
 
 const port = process.env.PORT;
 app.listen(port, () => {
